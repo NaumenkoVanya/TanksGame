b8d0328acd966ff030fd69d126bc7824d08f6694
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2023-03-07 00:33:55 +0300

    Background

--- TanksGame/TanksGame/Views/GameScene.swift
+++ TanksGame/TanksGame/Views/GameScene.swift
@@ -5,15 +5,23 @@
 //  Created by Ваня Науменко on 6.03.23.
 //
 
-import UIKit
-import SpriteKit
 import GameplayKit
+import SpriteKit
+import UIKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var backgroundNode: SKNode!
+    var tank: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+//
+//        let background = SKSpriteNode(imageNamed: "Pole")
+//        background.position = CGPoint(x: 0, y: 0)
+//        addChild(background)
+     
+        backgroundNode = self.childNode(withName: "Pole")
+        let tank2 = SKAction.move(by: CGVector(dx: 0, dy: -1000), duration: 0)
         
     }
 }
--- TanksGame/TanksGame/Views/GameViewController.swift
+++ TanksGame/TanksGame/Views/GameViewController.swift
@@ -5,15 +5,38 @@
 //  Created by Ваня Науменко on 6.03.23.
 //
 
-import UIKit
-import SpriteKit
 import GameplayKit
+import SpriteKit
+import UIKit
 
 class GameViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        if let view = view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 }
