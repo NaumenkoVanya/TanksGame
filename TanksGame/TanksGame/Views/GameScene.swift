b93268d405254b8c39a6851247ae06213375123f
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2023-03-06 23:18:04 +0300

    add acepty

---
+++ TanksGame/TanksGame/Views/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  TanksGame
+//
+//  Created by Ваня Науменко on 6.03.23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var backgroundNode: SKNode!
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ TanksGame/TanksGame/Views/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  TanksGame
+//
+//  Created by Ваня Науменко on 6.03.23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
